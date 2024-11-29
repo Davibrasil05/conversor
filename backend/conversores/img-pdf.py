44cc8be3d5bebcef91b79068d1aff51e0e026525
ThiagoMASS <thiago.limamassa2017@gmail.com>
2024-11-29 16:47:23 -0300

    ajustei a parte de converter direto quando envia do front-end

--- backend/conversores/img-pdf.py
+++ backend/conversores/img-pdf.py
@@ -1,12 +1,15 @@
 from PIL import Image
+from fpdf import FPDF
+import sys
 
-image_path= 'conversor/assets/image.png'
-image = Image.open(image_path)
+def convert_image_to_pdf(image_path, output_pdf_path):
+    image = Image.open(image_path)
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.image(image_path, x=0, y=0, w=210, h=297)  # Ajuste para o tamanho A4
+    pdf.output(output_pdf_path)
 
-
-image = image.convert('RGB')
-
-pdf_path = 'output.pdf'
-image.save(pdf_path,"PDF")
-
-print(f"Imagem convertida para PDF com sucesso! PDF salvo em: {pdf_path}")
+if __name__ == '__main__':
+    input_image = sys.argv[1]  # Caminho para a imagem
+    output_pdf = sys.argv[2]  # Caminho para salvar o PDF
+    convert_image_to_pdf(input_image, output_pdf)
